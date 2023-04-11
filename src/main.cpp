6a444ce9e3f3ddf220378a52b65ea16ddea84ad2
pawelbo231 <pawelbo231@gmail.com>
2023-04-11 15:01:07 +0200

    minor change in starting game

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@
 int main()
 {
 	int players;
-	std::cout << "0 - play with CPU" << std::endl
+	std::cout << "0 - two CPU players" << std::endl
 			  << "1 - two players" << std::endl
-			  << "2 - two CPU players" << std::endl;
+			  << "2 - play with CPU" << std::endl;
 	std::cin >> players;
 	Player *player1, *player2;
 	switch (players) {
 	case 0:
-		player1 = new PlayerHuman;
+		player1 = new PlayerCPU;
 		player2 = new PlayerCPU;
 		break;
 	case 1:
@@ -20,9 +20,6 @@ int main()
 		player2 = new PlayerHuman;
 		break;
 	case 2:
-		player1 = new PlayerCPU;
-		player2 = new PlayerCPU;
-		break;
 	default:
 		player1 = new PlayerHuman;
 		player2 = new PlayerCPU;
