fb3bc282d463e1de26d6a0c74e91efd92c0764e4
pawelbo231 <pawelbo231@gmail.com>
2023-04-13 16:09:44 +0200

    order of methods in header

--- inc/TicTacToe.h
+++ inc/TicTacToe.h
@@ -8,13 +8,13 @@ public:
 	typedef int PlayerID;
 	TicTacToe();
 	TicTacToe(const int numberOfHumanPlayers);
-	bool isRunning();
 	static void intro();
 	static std::string getInputFromConsole();
 	void terminate();
 	void step();
 	void clearConsole();
 	void printBoard();
+	bool isRunning();
 
 private:
 	bool m_running { true };
@@ -22,18 +22,18 @@ private:
 	bool m_allowed[9];
 	std::vector<std::unique_ptr<Player>> m_players;
 	PlayerID m_currentPlayer { 0 };
+	void initGame();
+	void initializeAllowed();
+	void initializeBoard();
 	bool isGameWon();
 	bool isMoveAllowed(const int field);
-	void takeFieldOnBoard(const int field, const char sign);
-	void nextPlayer();
-	char returnPlayerSign(const PlayerID player);
+	bool areThereFreeFields();
 	bool checkCol(const int);
 	bool checkRow(const int);
 	bool checkAllCols();
 	bool checkAllRows();
 	bool checkDiagonals();
-	void initGame();
-	void initializeAllowed();
-	void initializeBoard();
-	bool areThereFreeFields();
+	void takeFieldOnBoard(const int field, const char sign);
+	void nextPlayer();
+	char returnPlayerSign(const PlayerID player);
 };
